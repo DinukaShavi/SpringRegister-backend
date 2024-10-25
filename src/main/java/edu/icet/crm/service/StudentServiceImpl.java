bca72c1091fb1091a255b723d8a3b19853f86049
Dinuka Shavinda <dinukashavinda20@gmail.com>
2024-10-25 20:17:34 +0530

    object mapper update

--- src/main/java/edu/icet/crm/service/StudentServiceImpl.java
+++ src/main/java/edu/icet/crm/service/StudentServiceImpl.java
@@ -1,5 +1,6 @@
 package edu.icet.crm.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.icet.crm.entity.StudentEntity;
 import edu.icet.crm.model.Student;
 import edu.icet.crm.repository.StudentRepository;
@@ -15,14 +16,12 @@ public class StudentServiceImpl implements StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Override
     public StudentEntity saveStudent(Student student) {
-        StudentEntity studentEntity = new StudentEntity();
-        studentEntity.setName(student.getName());
-        studentEntity.setAge(student.getAge());
-        studentEntity.setAddress(student.getAddress());
-        studentEntity.setGuardianName(student.getGuardianName());
-        studentEntity.setGuardianContact(student.getGuardianContact());
+        StudentEntity studentEntity = objectMapper.convertValue(student, StudentEntity.class);
         return studentRepository.save(studentEntity);
     }
 
@@ -36,12 +35,9 @@ public class StudentServiceImpl implements StudentService {
         Optional<StudentEntity> optionalStudent = studentRepository.findById(id);
         if (optionalStudent.isPresent()) {
             StudentEntity existingStudent = optionalStudent.get();
-            existingStudent.setName(student.getName());
-            existingStudent.setAge(student.getAge());
-            existingStudent.setAddress(student.getAddress());
-            existingStudent.setGuardianName(student.getGuardianName());
-            existingStudent.setGuardianContact(student.getGuardianContact());
-            return studentRepository.save(existingStudent);
+            StudentEntity updatedStudent = objectMapper.convertValue(student, StudentEntity.class);
+            updatedStudent.setId(existingStudent.getId());
+            return studentRepository.save(updatedStudent);
         } else {
             throw new RuntimeException("Student not found with id: " + id);
         }
